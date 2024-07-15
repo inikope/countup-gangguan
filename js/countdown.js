bfe589e38d12b052bb4922b5d5d5907536eaf62b
inikope <59614495+inikope@users.noreply.github.com>
2024-07-15 19:42:08 +0700

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,23 @@
+{
+    var start = new Date;
+    start.setHours(19, 34, 0); // 11pm
+    start.setDate(11)
+  
+    function pad(num) {
+      return ("0" + parseInt(num)).substr(-2);
+    }
+  
+    function tick() {
+      var now = new Date;
+
+      var remain = ((now - start) / 1000);
+      var hh = pad((remain / 60 / 60));
+      var mm = pad((remain / 60) % 60);
+      var ss = pad(remain % 60);
+      document.getElementById('countdown').innerHTML =
+        hh + ":" + mm + ":" + ss;
+      setTimeout(tick, 1000);
+    }
+  
+    document.addEventListener('DOMContentLoaded', tick);
+}
