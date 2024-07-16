c0ad8edcaeea1ee5a40e8861c98b45cd452fcd1b
inikope <59614495+inikope@users.noreply.github.com>
2024-07-16 07:57:17 +0700

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -6,17 +6,21 @@
     function pad(num) {
       return ("0" + parseInt(num)).substr(-2);
     }
+
+    function pads(num) {
+      return ("0" + parseInt(num)).substr(-3);
+    }
   
     function tick() {
       var now = new Date;
 
       var remain = ((now - start) / 1000);
-      var hhh = pad((remain / 100 / 60 / 60));
+      var hhh = pads((remain / 100 / 60 / 60));
       var hh = pad((remain / 60 / 60));
       var mm = pad((remain / 60) % 60);
       var ss = pad(remain % 60);
       document.getElementById('countdown').innerHTML =
-        hhh + "" + hh + ":" + mm + ":" + ss;
+        hhh + "" + hh + " : " + mm + " : " + ss;
       setTimeout(tick, 1000);
     }
   
